621ddc61278e18f8a7b4913669429550a1517ac9
creeper8123 <anthgb@icloud.com>
2026-04-27 01:59:28 -0400

    Aluminum powder can be melted Fixed all the textures to the less pure-white aluminum (ooooopsss sorry honnnn) (texture still exists but is unused)

--- sillyaluminum/Code/ItemNuggetSodium.cs
+++ sillyaluminum/Code/ItemNuggetSodium.cs
@@ -3,21 +3,30 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 using Vintagestory.API.Datastructures;
+using System.Security.Cryptography;
 
-internal class ItemNuggetSodium : Item{
+internal class ItemNuggetSodium : ItemNugget{
     public override void OnGroundIdle(EntityItem entityItem){
-	if (!entityItem.Swimming || api.Side != EnumAppSide.Server)
-	{
-		return;
-	}
-	JsonObject attributes = Attributes;
-	if (attributes != null && attributes.IsTrue("explodeInWater"))
-	{
+		if (api.Side != EnumAppSide.Server){
+			return;
+		}
+
+		if(!entityItem.Swimming){
+			if(entityItem.Attributes.GetAsBool("burnOnWaterExit")){
+				entityItem.Attributes.RemoveAttribute("burnOnWaterExit");
+				entityItem.IsOnFire = true;
+				if(GetTemperature(entityItem.World, entityItem.Itemstack) < 800){
+					SetTemperature(entityItem.World, entityItem.Itemstack, 800);
+				}
+			}
+			return;
+		}
+
 		if (api.World.Rand.NextDouble() < 0.1){
 			api.World.SpawnCubeParticles(entityItem.Pos.XYZ, entityItem.Itemstack.Clone(), 0.1f, 80, 0.3f);
             //api.World.SpawnParticles(2, 255, entityItem.Pos.XYZ, entityItem.Pos.XYZ, )
-            ((IServerWorldAccessor)api.World).CreateExplosion(entityItem.Pos.AsBlockPos, EnumBlastType.EntityBlast, 1.0, 2.0, 1f);
-			entityItem.Ignite();
+            ((IServerWorldAccessor)api.World).CreateExplosion(entityItem.Pos.AsBlockPos, EnumBlastType.EntityBlast, 0.0, 2.5, 1f);
+			entityItem.Attributes.SetBool("burnOnWaterExit", true);
 			if(api.World.Rand.NextDouble() < 0.25){
 				entityItem.Die();
 			}else{
@@ -25,10 +34,8 @@ internal class ItemNuggetSodium : Item{
 				entityItem.Pos.Motion.Z += (api.World.Rand.NextDouble() - 0.5) * 0.25;
 				entityItem.Pos.Motion.Y += (api.World.Rand.NextDouble() * 0.5) + 0.25;
 			}
-		}
-		else if (api.World.Rand.NextDouble() < 0.2){
+		}else if (api.World.Rand.NextDouble() < 0.2){
 			api.World.SpawnCubeParticles(entityItem.Pos.XYZ, entityItem.Itemstack.Clone(), 0.1f, 2, 0.2f + (float)api.World.Rand.NextDouble() / 5f);
 		}
 	}
-}
 }
